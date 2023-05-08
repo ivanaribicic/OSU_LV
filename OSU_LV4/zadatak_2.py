8d584fab42527791fd94c6c7a3eaf7b5b60eb9ae
Ivana Ribičić <116194973+ivanaribicic@users.noreply.github.com>
2023-05-08 18:16:32 +0200

    zadatak_2.py

--- OSU_LV4/zadatak_2.py
+++ OSU_LV4/zadatak_2.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('data_C02_emission.csv')
 
 data = data.drop(['Make', 'Model'], axis=1)
-
 ohe = OneHotEncoder()
 X_encoded = ohe.fit_transform(data[['Fuel Type']]).toarray()
 
+#a)
 input_variables = ['Fuel Consumption City (L/100km)',
                    'Fuel Consumption Hwy (L/100km)',
                    'Fuel Consumption Comb (L/100km)',
@@ -31,23 +31,47 @@ y = data[output_variable].to_numpy()
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=1)
 
+#b)
+fuel_consumption_train = X_train[:,1]
+fuel_consumption_test = X_test[:,1]
+
+plt.scatter(fuel_consumption_train, y_train, c='blue', s=2)
+plt.scatter(fuel_consumption_test, y_test, c='red', s=2)
+plt.legend(['Training data', 'Test data'])
+plt.show()
+
+#d)
 linearModel = lm.LinearRegression()
 linearModel.fit(X_train, y_train)
 
-y_test_p = linearModel.predict(X_test)
 
+print('Parametri modela: ', linearModel.coef_)
+
+#e)
+y_test_p = linearModel.predict(X_test)
+plt.scatter(y_test, y_test_p, c='blue', s=2)
+plt.xlabel("Stvarna vrijednost")
+plt.ylabel("Izracunata vrijednost")
+plt.show()
+
+#f)
 print('Apsolutna pogreska: ', mean_absolute_error(y_test, y_test_p))
 print('Apsolutna postotna pogreska: ',
       metrics.mean_absolute_percentage_error(y_test, y_test_p))
 print('Apsolutna kvadratna pogreška: ', metrics.mean_squared_error(y_test, y_test_p))
 print('Koeficijent determinacije: ', metrics.r2_score(y_test, y_test_p))
 
-MAX_ERROR = metrics.max_error(y_test, y_test_p)
+#maksimalna pogreška
+errArray = abs(y_test_p - y_test)
+id = np.argmax(errArray)
 
-data["difference"] = abs(data['CO2 Emissions (g/km)'] - MAX_ERROR)
-car = data[data["difference"] == data["difference"].max()]
+print("Najveće odstupanje iznosi (g/km): ")
+print(errArray[id,0])
 
-print("Najveće odstupanje iznosi:", round(data["difference"].max(), 2), "g/km")
 print("Najveće odstupanje je za automobil:")
-print(car)
+print(data['Model'].to_numpy()[id])
+
+
+
+
 
